171e8202494937c5e5b91045bf8574a13467ce30
nithishvelumani15 <70762028+nithishvelumani15@users.noreply.github.com>
2026-05-04 13:36:04 +0530

    Update embedder.py

--- Rag With Langchain/embedder.py
+++ Rag With Langchain/embedder.py
@@ -13,27 +13,17 @@ EMBED_MODEL = "BAAI/bge-small-en-v1.5"
 
 FOLDER = r"C:\Users\nithi\Desktop\My Projects\Learning\RAG Tuturial\Gen-AI-Learning-Repo\Rag With Langchain\HR_Policy_Documents"
 def build_index():
-    # Step 1 — Wipe existing ChromaDB
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
-        print("Existing index wiped")
-
-    # Step 2 — Load documents
+        print("Existing index deleted")
     docs = load_documents(FOLDER)
-
-    # Step 3 — Chunk documents
     chunks = create_chunks(docs)
-
-    # Step 4 — Embed and store
     embeddings = HuggingFaceEmbeddings(model_name=EMBED_MODEL)
     vectorstore = Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
         persist_directory=CHROMA_PATH
     )
-
-    print(f"Index built successfully: {vectorstore._collection.count()} chunks stored")
-    print(f"ChromaDB saved at: {CHROMA_PATH}")
-
+    print("success)
 if __name__ == "__main__":
     build_index()
